814c999a2f9e2ba535183a97ff81bc25ee21d93f
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-03-06 18:15:13 +0100

    reset

--- js/script.js
+++ js/script.js
@@ -1,35 +1,16 @@
 // esercizio di oggi: calcolo del prezzo del biglietto del treno
-// cartella/repo js-biglietto-treno
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+// cartella/repo js-biglietto-treno-form
+// Descrizione:
+// Scrivere un programma che chieda all’utente:
+// - Il numero di chilometri da percorrere
+// - Età del passeggero
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
+// MILESTONE 1:
+// Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+// MILESTONE 2:
+// Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+// Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
-// ACQUISIZIONE DATI
-// chiedere quanti km si vuole percorrere
-const userDistance = parseInt(prompt('Quanti km devi percorrere?'));
-console.log('Distanza da percorrere: ' + userDistance + ' km');
-// chiedere quanti anni ha il passeggero
-const userAge = parseInt(prompt('Quanti anni ha il passeggero?'));
-console.log('L\'età del passeggero è: ' + userAge + ' anni');
-
-// LOGICA
-// calcolare il prezzo tot del viaggio in base ai km e all'età
-// prima calcolo il totale senza sconto:
-const fullPrice = userDistance * 0.21;
-console.log('Il prezzo intero è di: ' + fullPrice + ' €');
-// ora calcolo il prezzo e se c'è lo sconto:
-let finalPrice;
-if (userAge > 65) {
-    finalPrice = (fullPrice * 0.6).toFixed(2);
-} else if (userAge < 18) {
-    finalPrice = (fullPrice * 0.8).toFixed(2);
-} else {
-    finalPrice = fullPrice.toFixed(2);
-}
-console.log(finalPrice);
-// OUTPUT
-// prezzo in €!!!
-alert('Ti costerà: ' + finalPrice + ' € ' + 'per percorrere: ' + userDistance + ' km!!! Buon viaggio, spendaccione!');
